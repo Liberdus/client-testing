2ad6e2c9dec43bbc73dc5cab1b28387c8f2fdbec
Chris Freels <cafreels@gmail.com>
2025-06-11 16:20:28 -0500

    add retries and screenshots

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -32,6 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: process.env.CI ? true : false,  
+    screenshot: 'on-first-failure',
     launchOptions: { slowMo: 1000 }
   },
 
