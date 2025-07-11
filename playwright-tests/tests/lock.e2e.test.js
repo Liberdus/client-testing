ab8dab85923329eff8862cb43d28466c9a50c8dd
Chris Freels <cafreels@gmail.com>
2025-07-11 11:09:39 -0500

    add test to verify account cannot be unlocked with incorrect password

--- playwright-tests/tests/lock.e2e.test.js
+++ playwright-tests/tests/lock.e2e.test.js
@@ -32,6 +32,37 @@ test('Lock and Unlock Account', async ({ browser, browserName}) => {
     await expect(page.locator('#header .app-name')).toHaveText(username);
 });
 
+test('Should not unlock with wrong password', async ({ browser, browserName}) => {
+    // 1 create a user
+    const username = generateUsername(browserName);
+    const ctx = await browser.newContext();
+    const page = await ctx.newPage();
+    await createAndSignInUser(page, username);
+
+    // 2 lock the account
+    await page.click('#toggleMenu');
+    await expect(page.locator('#menuModal')).toBeVisible();
+    await page.click('#openLockModal');
+    await expect(page.locator('#lockModal')).toBeVisible();
+    const password = 'password';
+    await page.fill('#newPassword', password);
+    await page.fill('#confirmNewPassword', password);
+    await page.click('#lockForm button[type="submit"]');
+    await expect(page.locator('.toast.success.show')).toBeVisible({ timeout: 15_000 });
+
+    // 3 sign out
+    await page.click('#handleSignOut');
+    await expect(page.locator('#welcomeScreen')).toBeVisible();
+
+    // 4 attempt to sign with wrong password
+    await page.click('#signInButton');
+    await expect(page.locator('#unlockModal')).toBeVisible();
+    await page.fill('#password', password + 'wrong');
+    await page.click('#unlockForm button[type="submit"]');
+    await expect(page.locator('.toast.error.show', { hasText: /invalid password/i })).toBeVisible({ timeout: 10_000 });
+    await expect(page.locator('#unlockModal.active')).toBeVisible();
+});
+
 test('Change Lock Password', async ({ browser, browserName}) => {
     // 1 create a user
     const username = generateUsername(browserName);
